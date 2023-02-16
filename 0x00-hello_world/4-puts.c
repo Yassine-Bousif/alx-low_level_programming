500ca39643d5c5606ea9bb7fd8c1818cca8b0ebf
Yassine-Bousif <yassinstare@gmail.com>
2023-02-16 15:14:46 -0800

    hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /*
- * the function puts is similar to printf
+ * main - puts function
+ * Return: 0 (Success)
  */
-int main(void) 
+int main (void) 
 {
-	puts("Programming is like building a multilingual puzzle /n");
-	return(0); 
+	puts("Programming is like building a multilingual puzzle");
+	return (0); 
 }
