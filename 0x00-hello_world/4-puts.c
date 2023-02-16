8055d9e3ed459b2b23cef244645e3f1c4aadd996
Yassine-Bousif <yassinstare@gmail.com>
2023-02-16 15:20:08 -0800

    hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /*
- * main - puts function
+ * main is empty 
+ * puts function
  * Return: 0 (Success)
  */
-int main(void) 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0); 
+	return (0);
 }
