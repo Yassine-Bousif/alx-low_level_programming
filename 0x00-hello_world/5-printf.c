5cf2be92882e76e9af199101e31dc49549a727c8
Yassine-Bousif <yassinstare@gmail.com>
2023-02-16 15:32:43 -0800

    hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - the printf func
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,")
+	return(0)
+}
