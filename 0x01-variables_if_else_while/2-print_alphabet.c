28d8ec8aaa9feec229417439a00a3eaa6c1f7e97
Yassine-Bousif <yassinstare@gmail.com>
2023-02-17 15:58:24 -0800

    from a to z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * THIS IS a program that prints the alphabet in lowercase.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
