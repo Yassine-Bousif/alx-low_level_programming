a8b1a18ba0f77eb24be53d1be0a87c520cae94d5
Yassine-Bousif <yassinstare@gmail.com>
2023-02-17 16:02:14 -0800

    from a to z in lowercase then uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * THIS IS A program that prints alphabet in lowercase then uppercase
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
