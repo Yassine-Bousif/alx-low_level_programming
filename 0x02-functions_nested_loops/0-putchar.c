40ef694cb8998d76b93f2d571a1b956be0f3e520
Yassine-Bousif <yassinstare@gmail.com>
2023-02-21 13:08:18 -0800

    _put char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * display _putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
+}
