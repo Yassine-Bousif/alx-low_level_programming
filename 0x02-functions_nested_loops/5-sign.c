b950d35e4895ee1f0bad2d70fd493e6441b760e7
Yassine-Bousif <yassinstare@gmail.com>
2023-02-21 17:26:28 -0800

    stay positive-don’t-be-negative-or-stay-neutral

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* print_sign - function that prints sign of a number +/-/0
+* @n: number to be verified
+* Return: 0 if entered num is 0, 1 if num is positive, 
+* -1 if num is negative
+*/
+
+int print_sign(int n)
+{
+	if (n == 0)
+		{
+		_putchar('0');
+		return (0);
+		}
+	else if (n > 0)
+		{
+		_putchar('+');
+		return (1);
+		}
+	else
+		{
+		_putchar('-');
+		return (-1);
+		}
+}
