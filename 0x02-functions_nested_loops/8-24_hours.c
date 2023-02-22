a673f17b17aab8c10d485da8966985acf4a94a97
Yassine-Bousif <yassinstare@gmail.com>
2023-02-21 18:09:30 -0800

    I'm agent YsnLogan, and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * jack_bauer - a function that prints every minute of the day
+ * from 00:00 to 23:59
+*/
+
+void jack_bauer(void)
+{
+	int min, hr;
+
+	for (hr = 0; hr <= 23; hr++)
+	{
+		for (min = 0; min <= 59; min++)
+			{
+			_putchar (a / 10 + '0');
+			_putchar (a % 10 + '0');
+			_putchar (':');
+			_putchar (b / 10 + '0');
+			_putchar (b % 10 + '0');
+			_putchar ('\n');
+			}
+
+
+	}
+
+}
