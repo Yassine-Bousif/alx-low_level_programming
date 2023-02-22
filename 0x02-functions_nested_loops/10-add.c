c00d21003617ea7fe3fe199d8f23d8c273d2525a
Yassine-Bousif <yassinstare@gmail.com>
2023-02-21 18:27:47 -0800

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* add - a function that sums up two numbers
+* @x:first integer
+* @y:second integer
+* Return: Always 0
+*/
+
+int add(int x, int y)
+	{
+	return (x + y);
+	}
