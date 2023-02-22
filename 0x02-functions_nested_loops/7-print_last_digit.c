6b3d4f43423dfe5f1b92546494ef156417f0c2ba
Yassine-Bousif <yassinstare@gmail.com>
2023-02-21 17:56:24 -0800

    Last Digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,26 +3,26 @@
  *
  * print_last_digit - prints last digit of a number
  * @x: takes inputed num
- * Return: last-num
+ * Return: last_num
  *
 */
 
 int print_last_digit(int x)
 {
-	int last-num;
+	int last_num;
 
 	if (x < 0)
 		{
-		last-num = -1 * (x % 10);
-		_putchar (last-num + '0');
-	return (last-num);
+		last_num = -1 * (x % 10);
+		_putchar (last_num + '0');
+	return (last_num);
 		}
 
 	else
 		{
-		last-num = (x % 10);
-		_putchar (last-num + '0');
-	return (last-num);
+		last_num = (x % 10);
+		_putchar (last_num + '0');
+	return (last_num);
 		}
 
 }
