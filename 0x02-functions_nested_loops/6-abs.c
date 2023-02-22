41e42e65c198f28fa08578de19e3d25a2535e71b
Yassine-Bousif <yassinstare@gmail.com>
2023-02-21 17:41:45 -0800

    The Absolute Value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - absolute value function
+ * compute the absolute value of an integer
+ * @n: yje targeted num
+ * Return: -n or n
+ *
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		{
+		return (-n);
+		}
+	else
+		{
+		return (n);
+		}
+}
