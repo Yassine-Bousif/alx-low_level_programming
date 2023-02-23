d5392a2ba8d2effb8a833cd86b8af52d2783b12e
Yassine-Bousif <yassinstare@gmail.com>
2023-02-23 14:58:27 -0800

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,16 +3,14 @@
 /**
  * print_numbers - a func that prints numbers from 0 to 9
  *
- * Return: Always 0
+ * Return: void
  */
 
 int print_numbers(void)
 {
-	int x;
+	int i;
 
-	for (x = 0; x < 10; x++)
-	{
-		_putchar(x + '0');
-	}
+	for (i = 0; i < 10; i++)
+		_putchar(i + '0');
 	_putchar('\n');
 }
