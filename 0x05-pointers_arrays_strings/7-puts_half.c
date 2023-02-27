14fac9094dee063176bed0866f0e17561f1e2a8b
Yassine-Bousif <yassinstare@gmail.com>
2023-02-27 14:53:57 -0800

    Half of Half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_half -  prints half of a string
+ * @str: string to be printed
+ */
+
+void puts_half(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++);
+
+	x++;
+
+	for (x /= 2; str[x] != '\0'; x++)
+	{
+		_putchar(str[x]);
+	}
+	_putchar('\n');
+}
