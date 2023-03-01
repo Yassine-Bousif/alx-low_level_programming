36aa2f348cd77b16e2ad7dd2b6bc664b73c69d4f
Yassine-Bousif <yassinstare@gmail.com>
2023-03-01 00:54:02 -0800

    strcat0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _strcat - concatenates two strings
+* @dest: destination string
+* @src: source string
+* return: pointer to resulting string dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int x, y;
+	for (x = 0; dest[x]; x++)
+		;
+	for (y = 0; src[y]; y++)
+	dest[x + y] = src[y];
+	dest[x + y] = '\0';
+	return (dest);
+}
