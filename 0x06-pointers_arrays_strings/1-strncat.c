f7f0154969b495d2b53241805f7157861be7d1ea
Yassine-Bousif <yassinstare@gmail.com>
2023-03-01 15:34:51 -0800

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,11 @@
 #include"main.h"
 
 /**
- * _strncat - concatenates two strings
+ * *_strncat - A function that concatenates two strings
  * @dest: destination string
  * @src: source string
- * @n: number of bytes to concatenate
- * return: pointer to resulting string dest
+ * @n: the number of bytes from src
+ * return: a pointer to resulting string dest
  */
 
 char *_strncat(char *dest, char *src, int n)
