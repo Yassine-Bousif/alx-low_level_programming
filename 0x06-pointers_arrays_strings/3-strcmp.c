cf5a63b2ed562b54966a3595a18752f45bd7a3ec
Yassine-Bousif <yassinstare@gmail.com>
2023-03-01 15:59:38 -0800

    strcmp3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,7 +14,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int x;
 
-	for (x = 0; s1[x] && s2[x] && s1[x] == s2[x]; i++)
+	for (x = 0; s1[x] && s2[x] && s1[x] == s2[x]; x++)
 		;
 	return (s1[x] - s2[x]);
 }
