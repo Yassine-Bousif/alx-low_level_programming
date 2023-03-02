95a627880f4f132b68bd8d82282cd2e5b937980d
Yassine-Bousif <yassinstare@gmail.com>
2023-03-01 16:53:25 -0800

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *rot13 - Encodes a string using rot13.
+ *
+ * @s: the string subject of encoding
+ *
+ * Return: encoded string
+ */
+
+char *rot13(char *s)
+{
+	int x, y;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; a[y] != '\0'; y++)
+		{
+			if (s[x] == a[y])
+			{
+				s[x] = b[y];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
