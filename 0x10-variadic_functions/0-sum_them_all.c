874330da218d39bdb33a858bcf284fa5e84b3d16
Yassine-Bousif <yassinstare@gmail.com>
2023-03-23 06:14:39 -0700

    fixing variadic_functions.h

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "variadic_functions.h"
 
 /**
  * sum_them_all - calculates the sum of all its parameters
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "variadic_functions.h"
 
 /**
  * print_numbers - prints numbers given as parameters
